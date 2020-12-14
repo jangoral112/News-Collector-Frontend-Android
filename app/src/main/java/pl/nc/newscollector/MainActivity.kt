3f59069027080b8a4f2d208a1a4029826e2fbe96
Reszkettle <reszkettle@gmail.com>
2020-12-14 20:34:46 +0100

    Initial commit

---
+++ app/src/main/java/pl/nc/newscollector/MainActivity.kt
@@ -0,0 +1,11 @@
+package pl.nc.newscollector
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

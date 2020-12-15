3600d19ea9fb6fbaa340f3a70a22c2ce5f7c6f79
Reszkettle <reszkettle@gmail.com>
2020-12-15 16:40:52 +0100

    Added serialization of Article. Added implementation of gson and fuel. Added internet permission.

--- app/src/main/java/pl/nc/newscollector/fragments/ArticlesFragment.kt
+++ app/src/main/java/pl/nc/newscollector/fragments/ArticlesFragment.kt
@@ -23,10 +23,10 @@ class ArticlesFragment : Fragment(R.layout.fragment_articles) {
 
 
     private fun getSampleData(): ArrayList<Article> {
-        var article = ArrayList<Article>()
-        article.add(Article("Kradzieże na ulicy Matejki", "12.12.12", "asbasbas Bla bla balb asbabasbasbas", "https://www.youtube.com/watch?v=hbMqd0XRN34&t=616s", "covid", "CNN", "Europe"))
-        article.add(Article("Kotlin lang hahaha", "15.12.12", "Bla bla balb asbabasbasbas", "https://discuss.kotlinlang.org/t/ternary-operator/2116/2", "covid", "Onet", "Polacy"))
-        article.add(Article("Pepper fajna hahaha stronga", "12.12.12", "Bla bla balb asbabasbasbas", "https://www.pepper.pl/", "covid", "WP", "PiS"))
-        return article
+        var articles = ArrayList<Article>()
+        articles.add(Article("Castro: USA celowo nie zapobiegły atakom terrorystów", "15.12.2020", "Stany Zjednoczone celowo nie zapobiegły atakom na swoim terytorium, żeby móc usprawiedliwić wojnę z terroryzmem - napisał kubański przywódca Fidel Castro w artykule, opublikowanym na łamach dziennika młodzieży komunistycznej \"Juventud Rebelde\".", "https://www.wp.pl/?service=wiadomosci.wp.pl&paid=6037087318049921&c=96&src01=f1e45", "Castro", "WP", "Wiadomości"))
+        articles.add(Article("Jak nie dać się oszukać kupując choinkę i wybrać najlepszą", "14.12.2020", "Wielu z nas stoi przed wyborem choinki. Jeśli już decydujemy się na żywe drzewko, warto wiedzieć, jak wybrać to najlepsze, które nie zgubi igieł po kilku dniach. Czym się kierować wybierając sprzedawcę? Jak rozpoznać, czy uczciwie sprzedaje swoje drzewka i skąd mieć pewność, że choinka jest świeżo ścięta?", "https://www.onet.pl/styl-zycia/onetkobieta/jak-nie-dac-sie-oszukac-kupujac-choinke-i-wybrac-najlepsza/8m1ye1d,2b83378a", "Christmas", "Onet", "Styl Życia"))
+        articles.add(Article("The incredible stories behind 5 of the world's most expensive watches", "15.12.2020", "A little over 30 years ago, Geneva's Antiquorum pioneered the modern watch auction with its \"Art of Patek Philippe\" sale. ", "https://edition.cnn.com/style/article/5-of-historys-most-expensive-watches/index.html", "Watch, Patek", "CNN", "Style"))
+        return articles
     }
 }
--- app/src/main/java/pl/nc/newscollector/models/Article.kt
+++ app/src/main/java/pl/nc/newscollector/models/Article.kt
@@ -1,12 +1,16 @@
 package pl.nc.newscollector.models
 
+import com.google.gson.annotations.SerializedName
+
+
 data class Article (
-    val title: String,
-    val date: String,
-    val description: String,
-    val link: String,
-    val keywords: String, // TODO Investigate if we provide user with keywords or single keyword
-    val websiteName: String,
-    val feedName: String,
-    var isExpanded: Boolean = false
+        @field:SerializedName("news_title") val title: String,
+        @field:SerializedName("publish_date") val date: String,
+        @field:SerializedName("news_description") val description: String,
+        @field:SerializedName("news_link") val link: String,
+        @field:SerializedName("keyword_value") val keywords: String, // TODO Investigate if we provide user with keywords or single keyword
+        @field:SerializedName("website_name") val websiteName: String,
+        @field:SerializedName("feed_name") val feedName: String,
+        var isExpanded: Boolean = false
+
 )
